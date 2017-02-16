70856404fa9065106dbd59c456205a5bc9d1e21b
Marlena Bowen <marlena@joinroot.com>
2017-02-16 18:21:08 -0500

    Completes the first step of the kata

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,17 +1,32 @@
 'use strict';
 
 export default class StringCalculator {
-  static add(n) {
-    let array = n.split(',');
-    let result;
-
+  static calculate(n) {
     if (n) {
-      array.forEach(number, () => {
-        result += number;
-      )};
-      return result - '';
+      return this.add(n);
     } else {
       return 0;
     }
+  }
+
+  static removeDelimeter(n) {
+    let delimeter = '';
+    if (n.includes(',')) {
+      delimeter = ',';
+    } else if (n.includes('\n')) {
+      delimeter = '\n';
+    }
+    const array = n.split(delimeter);
+    return array;
+  }
+
+  static add(n) {
+    const array = this.removeDelimeter(n);
+    let result = 0;
+
+    array.forEach((number) => {
+      result += parseInt(number);
+    });
+    return result;
   }
 }
--- test/string-calculator-test.js
+++ test/string-calculator-test.js
@@ -1,15 +1,36 @@
 import StringCalculator from '../src/string-calculator.js';
 
-describe.only('<StringCalculator />', () => {
-  it('returns 0 if no numbers are given', () => {
-    expect(StringCalculator.add('')).to.eq(0);
+describe('<StringCalculator />', () => {
+  context('where commas are the only delimeters', () => {
+    it('returns 0 if no numbers are given', () => {
+      expect(StringCalculator.calculate("")).to.eq(0);
+    });
+
+    it('returns correct int if given a single number string', () => {
+      expect(StringCalculator.calculate("3")).to.eq(3);
+    });
+
+    it('returns correct int if given a two number string', () => {
+      expect(StringCalculator.calculate("3,2")).to.eq(5);
+    });
+
+    it('returns correct int if given any amount of numbers', () => {
+      expect(StringCalculator.calculate("3,0,2,16,234,0")).to.eq(255);
+    });
   });
 
-  it('returns correct int if given a single number string', () => {
-    expect(StringCalculator.add('3')).to.eq(3);
+  context('where new lines are the only delimeters', () => {
+    expect(StringCalculator.calculate("3\n2\n10")).to.eq(15);
   });
 
-  it('returns correct int if given a two number string', () => {
-    expect(StringCalculator.add('3,2')).to.eq(5);
-  });
+  // context('where anything can be a delimeter', () => {
+  //   expect(StringCalculator.calculate("//;n1;2")).to.eq(3);
+  //   expect(StringCalculator.calculate("3/4/3")).to.eq(10);
+  // });
+  //
+  // context('where negative numbers are included', () => {
+  //   it('throws an exception', () => {
+  //     expect(StringCalculator.calculate("-2,-4")).to.eq("negatives not allowed: -2, -4");
+  //   });
+  // });
 });
