869dbca934c6cf5637e2bbc3840924238ed6f508
Marlena Bowen <marlena@joinroot.com>
2017-02-16 17:35:01 -0500

    tmp

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,5 +1,17 @@
 'use strict';
 
 export default class StringCalculator {
+  static add(n) {
+    let array = n.split(',');
+    let result;
 
+    if (n) {
+      array.forEach(number, () => {
+        result += number;
+      )};
+      return result - '';
+    } else {
+      return 0;
+    }
+  }
 }
--- test/string-calculator-test.js
+++ test/string-calculator-test.js
@@ -1,5 +1,15 @@
 import StringCalculator from '../src/string-calculator.js';
 
 describe.only('<StringCalculator />', () => {
+  it('returns 0 if no numbers are given', () => {
+    expect(StringCalculator.add('')).to.eq(0);
+  });
 
+  it('returns correct int if given a single number string', () => {
+    expect(StringCalculator.add('3')).to.eq(3);
+  });
+
+  it('returns correct int if given a two number string', () => {
+    expect(StringCalculator.add('3,2')).to.eq(5);
+  });
 });
